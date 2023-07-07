51bc7877b2dc169e073f173392d019c8cd9ca8cd
salino3 <flaviobcn012@gmail.com>
2023-07-07 09:27:36 +0200

    add hooks

--- src/pods/desc-form/desc-form.component.tsx
+++ src/pods/desc-form/desc-form.component.tsx
@@ -1,50 +1,37 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { cx } from '@emotion/css';
 import { Characters, GlobalContext, MyState } from '@/core';
 import * as classes from './desc-form.styles';
-import { SwitchRoutes } from '@/router';
+import { MyHooks } from '@/hooks';
 
 
 export const DescFormComponent: React.FC = () => {
 
+  
   const params = useParams();
-  const {createDescription, getCharacter, state} = React.useContext<MyState>(GlobalContext);
+  const { getCharacter, state} = React.useContext<MyState>(GlobalContext);
   const { characters, character } = state;
-
-  const navigate = useNavigate();
-
+  
+  
   React.useEffect(() => {
     getCharacter(Number(params?.id)); 
   }, [params?.id]);
-
-
+  
+  
   const [desc, setDesc] = React.useState<string | undefined>(
-      characters.find((character: Characters) => character.id === Number(params?.id))?.description
-      ?? ""
-    );
-
+    characters.find((character: Characters) => character.id === Number(params?.id))?.description
+    ?? "");
     
-  const handleChange = ( event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setDesc(event.target.value);
-  };
-
-
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (
-    event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      if(desc) {
-        createDescription(Number(params?.id), desc);
-        navigate(-1);
-      };
-  };
+  const { handleChangeDescForm, handleSubmitDescForm } = MyHooks({ params, desc, setDesc });
     
+
   return (
     <div className={classes.root}>
       <h1 className={classes.title}> Description Form</h1>
-      <form onSubmit={handleSubmit} className={classes.form}>
+      <form onSubmit={handleSubmitDescForm} className={classes.form}>
         <h2 className={classes.subTitle}>
-         <span>Name:</span> {character && character?.name} <span>- id: </span>
+          <span>Name:</span> {character && character?.name} <span>- id: </span>
           {character && character?.id}
         </h2>
         <label className={classes.lblDesc} htmlFor="description">
@@ -52,7 +39,7 @@ export const DescFormComponent: React.FC = () => {
         </label>
         <textarea
           className={classes.txtArea}
-          onChange={handleChange}
+          onChange={handleChangeDescForm}
           value={desc}
           placeholder="Text a description.."
           name="description"
